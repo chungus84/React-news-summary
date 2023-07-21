bda9bdb802f35e5a15dfab9ee3c9fb3c1eef038f
kenichi beveridge <kenbev84@gmail.com>
2023-07-21 16:54:35 +0100

    Initial project setup

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./tests/setup.js'],
-    testMatch: ['./tests/**/*.test.jsx'],
-    globals: true
-  }
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['./tests/setup.js'],
+        testMatch: ['./tests/**/*.test.jsx'],
+        coverage: {
+            provider: 'istanbul', // or 'v8'
+            reporter: ['text', 'json', 'html']
+        },
+
+        globals: true
+    }
 })
